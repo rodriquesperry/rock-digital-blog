0ffd98d7c618801df1e33776b427957ce508ac53
Rodriques <rodriques.perry@gmail.com>
2025-01-23 12:42:09 -0600

    Update: config/server.js host: env('HOST', '0.0.0.0')

--- config/server.js
+++ config/server.js
@@ -1,9 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "127.0.0.1"),
+  host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
   url: "https://rockdigital.agency",
-  // url: "http://127.0.0.1:1337",
 });
