5116804e1d57ed6b70c8732a5e9d27d4ebfccedb
Rodriques <rodriques.perry@gmail.com>
2025-01-23 09:54:03 -0600

    Update: config/database.js and config/env/production/database.js

--- config/database.js
+++ config/database.js
@@ -1,19 +1,22 @@
-const fs = require('fs');
+const fs = require("fs");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "mysql",
+module.exports = ({ env }) => {
+  return {
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: {
-        ca: fs.readFileSync(
-          "/Volumes/Rods Hard Drive/Rock Digital/ca-certificate.crt",
-        ),
+      client: "mysql",
+      connection: {
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        connectTimeout: 10000,
+        ssl: {
+          ca: fs.readFileSync(
+            "/Volumes/Rods Hard Drive/Rock Digital/ca-certificate.crt"
+          ),
+        },
       },
     },
-  },
-});
+  };
+};
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,47 @@
-const fs = require('fs');
+const fs = require("fs");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "mysql",
+// module.exports = ({ env }) => ({
+//   return {
+//   connection: {
+//     client: "mysql",
+//     connection: {
+//       host: env("DATABASE_HOST"),
+//       port: env.int("DATABASE_PORT"),
+//       database: env("DATABASE_NAME"),
+//       user: env("DATABASE_USERNAME"),
+//       password: env("DATABASE_PASSWORD"),
+//       connectTimeout: 10000,
+//       ssl: {
+//         ca: fs.readFileSync(
+//           "/Volumes/Rods Hard Drive/Rock Digital/ca-certificate.crt",
+//         ),
+//         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", true), // Use true for secure connections
+//       },
+//     },
+//     debug: false,
+//   },
+// }
+// });
+
+module.exports = ({ env }) => {
+  return {
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: {
-        ca: fs.readFileSync('/Volumes/Rods Hard Drive/Rock Digital/ca-certificate.crt'),
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", true), // Use true for secure connections
+      client: "mysql",
+      connection: {
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        connectTimeout: 10000,
+        ssl: {
+          ca: fs.readFileSync(
+            "/Volumes/Rods Hard Drive/Rock Digital/ca-certificate.crt"
+          ),
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", true), // Use true for secure connections
+        },
       },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+};
